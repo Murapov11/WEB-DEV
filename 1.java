e995840c5ba23924e272669136eaeccdd00fc8b2
Murapov11 <murapov.yerkhan.@gmail.com>
2023-03-08 13:08:43 +0600

    first version

--- 1.java
+++
@@ -1,5 +0,0 @@
-String s = "";
-while(num){
-    s = Integer.toString(num % 2) + s;
-    num /= 2;
-}
--- Lab3/ToDo App/script.js
+++ Lab3/ToDo App/script.js
@@ -1,27 +1,31 @@
-const form = document.querySelector("#form");
+const form = document.querySelector("#form"); // DOM objects
 const input = document.querySelector("#item");
 const list = document.querySelector("#list");
-form.addEventListener("submit", function(event) {
-  event.preventDefault();
+form.addEventListener("submit", function(event) { // anonymous function
+  event.preventDefault(); // When a form is submitted, the default behavior is for the page to refresh.
   addItem(input.value);
   input.value = "";
 });
+
 function addItem(text) {
   const item = document.createElement("li");
-  item.classList.add("item");
-  item.innerHTML = `
+  item.classList.add("item"); //  sets its class to item
+  item.innerHTML = ` 
     ${text}
     <input type="checkbox">
-    <button class="delete">&times;</button>`;
+    <button class="delete">DELETE</button>`; // HTML code of item element
 
     const checkbox = item.querySelector("input[type='checkbox']");
     checkbox.addEventListener("click", function() {
-        item.classList.toggle("done");
+      item.classList.toggle("done"); // (переключать) This method adds the done class to the item element
+      //if it is not already present,
+      // or removes it if it is already present.
     });
       
     const deleteButton = item.querySelector(".delete");
     deleteButton.addEventListener("click", function() {
         list.removeChild(item);
     });
-      list.appendChild(item);
+  
+    list.appendChild(item);
 }
